4f27aa17cf46989d42c96e402194831ee2568246
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-20 11:33:27 +0500

    добавил класс DocumentWorker и освоил 3 метода OpenDoc(), EditDoc(), SaveDoc()

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class DocumentWorker
+    {
+        public virtual void OpenDoc()
+        {
+
+        }
+
+        public virtual void EditDoc()
+        {
+
+        }
+
+        public virtual void SaveDoc()
+        {
+
+        }
+    }
+}
