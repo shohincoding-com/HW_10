9ffe0bb33c28e2828a1658ab3dd8cc2fa3ae8cec
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-20 11:57:53 +0500

    Добавил класс ProDocumentWorker и ExpertDocumentWorker также внутри класс создал нужные методи указанно на задачке

--- Program.cs
+++ Program.cs
@@ -14,17 +14,37 @@ namespace HW_10
     {
         public virtual void OpenDoc()
         {
-
+            Console.WriteLine("Документ открыт");
         }
 
         public virtual void EditDoc()
         {
-
+            Console.WriteLine("Редактирование документа доступно в версии Про");
         }
 
         public virtual void SaveDoc()
         {
-
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
+    }
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDoc()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+ 
+        public override void SaveDoc()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+ 
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDoc()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
         }
     }
 }
